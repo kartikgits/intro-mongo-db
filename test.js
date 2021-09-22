12a6f9f88a4b7c1ec3103cf420ae9bc715aa8c56
Kartikeya <kartik.kapparwan1@gmail.com>
2021-09-22 08:27:23 +0530

    Mongoose schema and basic query

--- test.js
+++ test.js
@@ -3,7 +3,22 @@ const mongoose = require('mongoose')
 const connect = () => mongoose.connect('mongodb://localhost:27017/whatever')
 
 const student = new mongoose.Schema({
-    firstName: String
+    firstName: {
+        type: String,
+        required: true
+    },
+    
+    info: {
+        school: {
+            type: String
+        },
+
+        shoeSize: {
+            type: Number
+        }
+    },
+
+    favoriteFoods: [{ type: String }]
 })
 
 const Student = mongoose.model('student', student)
@@ -11,6 +26,9 @@ const Student = mongoose.model('student', student)
 connect()
     .then(async connection => {
         const student = await Student.create({firstName: 'Tim'})
+        const found = await Student.find({ firstName: 'Tim' })
+        const foundById = await Student.findById('asdfdssdf')
+        const updated = await Student.findByIdAndUpdate('aslkdfjsd')
         console.log(student)
     })
     .catch(e => console.error(e))
